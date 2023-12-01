4019c4af9d5afad56ac140f9ac3db11bf5d53f1f
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-01 16:03:40 -0300

    Update movie genre in API endpoint

--- clase3/app.js
+++ clase3/app.js
@@ -1,21 +1,49 @@
 const express = require("express");
 const movies = require("./movies.json");
+const crypto = require("node:crypto");
 
 const app = express();
+app.use(express.json()); // Para que express entienda el body de las peticiones POST
 app.disable("x-powered-by"); // Para que no se vea que estamos usando express, MAS SEGURIDAD
 
 app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  if (genre) {
+    const moviesByGenre = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(moviesByGenre);
+  }
   res.json(movies);
 });
 
 app.get("/movies/:id", (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const movie = movies.find((movie) => movie.id === (id));
+  const movie = movies.find((movie) => movie.id === id);
 
-    if (movie) return res.json(movie);
+  if (movie) return res.json(movie);
 
-    res.status(404).json({ error: "Movie not found" });
+  res.status(404).json({ error: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const { title, year, director, duration, poster, genre, rate } = req.body;
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title: title,
+    year: year,
+    director: director,
+    duration: duration,
+    poster: poster,
+    genre: genre,
+    rate: rate ?? 0,
+  };
+  console.log(newMovie);
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
 });
 
 const PORT = process.env.PORT ?? 1234;

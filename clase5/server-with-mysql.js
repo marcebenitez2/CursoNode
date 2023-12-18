0eaee5cb9c6aa7510830b60703ec81ae35bf724a
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-18 18:43:52 -0300

    Update database URL and add comment

--- clase5/server-with-mysql.js
+++ clase5/server-with-mysql.js
@@ -2,3 +2,6 @@ import { createApp } from "./app.js";
 import { MovieModel } from "./models/mysql/movie.js";
 
 createApp({ MovieModel });
+
+
+// Ya instale planetScale, me loguee y cree la base de datos 

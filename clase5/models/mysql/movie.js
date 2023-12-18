6c28bf91b2321debb5e41a5f1d2f88515ccc47e1
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-18 13:19:29 -0300

    Refactor app.js and delete movie.js

---
+++ clase5/models/mysql/movie.js
@@ -0,0 +1,13 @@
+export class MovieModel {
+  static async getAll({ genre }) {
+    
+  }
+
+  static async getById({ id }) {}
+
+  static async create({ input }) {}
+
+  static async delete({ id }) {}
+
+  static async update({ id, input }) {}
+}

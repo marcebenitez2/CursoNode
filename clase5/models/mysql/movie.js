def7679d2d368f60aea6b5fb63f6ddd0869ace50
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-18 16:53:47 -0300

    Update movie genre in API endpoint

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -16,37 +16,140 @@ export class MovieModel {
     if (genre) {
       const lowerCaseGenre = genre.toLowerCase();
 
-      // get genre ids from database table using genre names
       const [genres] = await connection.query(
         "SELECT id, name FROM genres WHERE LOWER(name) = ?;",
         [lowerCaseGenre]
       );
 
-      // no genre found
       if (genres.length === 0) return [];
 
-      // get the id from the first genre result
       const [{ id }] = genres;
 
-      // get all movies ids from database table
-      // la query a movie_genres
-      // join
-      // y devolver resultados..
-      return [];
+      const [movies] = await connection.query(
+        "SELECT m.title, m.year, m.director, m.duration, m.poster, m.rate, m.id, g.name as genre " +
+          "FROM movies m " +
+          "INNER JOIN movie_genre mg ON m.id = mg.movie_id " +
+          "INNER JOIN genres g ON mg.genre_id = g.id " +
+          "WHERE g.id = ?;",
+        [id]
+      );
+
+      return movies;
     }
 
     const [movies] = await connection.query(
-      "SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie;"
+      "SELECT title, year, director, duration, poster, rate, id FROM movies;"
     );
 
     return movies;
   }
 
-  static async getById({ id }) {}
+  static async getById({ id }) {
+    const movies = await connection.query(
+      "SELECT title, year, director, duration, poster, rate, id FROM movies WHERE id = (?)",
+      [id]
+    );
 
-  static async create({ input }) {}
+    if (movies.length === 0) return null;
 
-  static async delete({ id }) {}
+    return movies[0];
+  }
 
-  static async update({ id, input }) {}
+  static async create({ input }) {
+    const {
+      genre,
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate,
+    } = input;
+  
+    console.log(input);
+  
+    const [uuidResult] = await connection.query("SELECT UUID() uuid;");
+    const [{ uuid }] = uuidResult;
+  
+    try {
+      // Para insertar la película
+      await connection.query(
+        "INSERT INTO movies (id, title, year, director, duration, poster, rate) VALUES (?, ?, ?, ?, ?, ?, ?);",
+        [uuid, title, year, director, duration, poster, rate]
+      );
+  
+      // Para insertar los géneros
+      for (const genreInput of genre) {
+        // Verificar si el género ya existe en la base de datos
+        const [genres] = await connection.query(
+          "SELECT id, name FROM genres WHERE LOWER(name) = ?;",
+          [genreInput.toLowerCase()]
+        );
+  
+        // Si no existe, lo creamos
+        if (genres.length === 0) {
+          await connection.query("INSERT INTO genres (name) VALUES (?);", [
+            genreInput,
+          ]);
+  
+          // Relacionar la película con el nuevo género
+          await connection.query(
+            "INSERT INTO movie_genre (movie_id, genre_id) VALUES (?, ?);",
+            [uuid, genreInput]
+          );
+        }
+        // Si ya existe, simplemente relacionamos la película con el género existente
+        else {
+          await connection.query(
+            "INSERT INTO movie_genre (movie_id, genre_id) VALUES (?, ?);",
+            [uuid, genres[0].id]
+          );
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  
+    const [movies] = await connection.query(
+      "SELECT title, year, director, duration, poster, rate, id id FROM movies WHERE id = (?)",
+      [uuid]
+    );
+  
+    return movies[0];
+  }
+  
+  static async delete({ id }) {
+
+
+    const [movies] = await connection.query(
+      "DELETE FROM movies WHERE id = ?;",
+      [id]
+    );
+
+    await connection.query(
+      "DELETE FROM movie_genre WHERE movie_id = ?;",
+      [id]
+    );
+    
+    return movies;
+  }
+
+  static async update({ id, input }) {
+    const {
+      genre: genreInput,
+      title,
+      year,
+      director,
+      duration,
+      poster,
+      rate,
+    } = input;
+
+    const [movies] = await connection.query(
+      "UPDATE movies SET title = ?, year = ?, director = ?, duration = ?, poster = ?, rate = ? WHERE id = ?;",
+      [title, year, director, duration, poster, rate, id]
+    );
+
+    return movies;
+  }
 }

653f7286bb5f5b0f60e085274f9798dce86a03f0
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-18 14:02:40 -0300

    Commented out local file system import and added MySQL import for MovieModel

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -1,6 +1,45 @@
+import mysql from "mysql2/promise";
+
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "moviesdb",
+};
+
+const connection = await mysql.createConnection(config);
+
 export class MovieModel {
   static async getAll({ genre }) {
-    
+    console.log("getAll");
+
+    if (genre) {
+      const lowerCaseGenre = genre.toLowerCase();
+
+      // get genre ids from database table using genre names
+      const [genres] = await connection.query(
+        "SELECT id, name FROM genres WHERE LOWER(name) = ?;",
+        [lowerCaseGenre]
+      );
+
+      // no genre found
+      if (genres.length === 0) return [];
+
+      // get the id from the first genre result
+      const [{ id }] = genres;
+
+      // get all movies ids from database table
+      // la query a movie_genres
+      // join
+      // y devolver resultados..
+      return [];
+    }
+
+    const [movies] = await connection.query(
+      "SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie;"
+    );
+
+    return movies;
   }
 
   static async getById({ id }) {}

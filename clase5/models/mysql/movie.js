83955053c866619ccf8b52bbefef658b524aba26
marcebenitez2 <marcebenitez0607@gmail.com>
2023-12-18 18:38:07 -0300

    Add dotenv configuration

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -1,13 +1,15 @@
 import mysql from "mysql2/promise";
 
-const config = {
+const DEFAULT_CONFIG = {
   host: "localhost",
   user: "root",
   password: "",
   database: "moviesdb",
 };
 
-const connection = await mysql.createConnection(config);
+const connectionString = process.env.DATABASE_URL ?? DEFAULT_CONFIG
+
+const connection = await mysql.createConnection(connectionString);
 
 export class MovieModel {
   static async getAll({ genre }) {
